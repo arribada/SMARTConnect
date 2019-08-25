54d9bdd70015b1e02dc23f07dc4730297e1953ca
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2019-08-25 17:45:41 +0300

    use the same env name naming as the upstream image
    POSTGRES_USER_FILE is when using a file
    POSTGRES_USER is when using an env var.
    
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- tomcat/entrypoint.sh
+++ tomcat/entrypoint.sh
@@ -3,12 +3,6 @@
 # Exit when any command fails
 #set -e
 
-mkdir -p /home/SMARTconnect/filestore/dataqueue/
-if [ $? -ne 0 ]; then
-    >&2 echo "Couldn't create dataque folder"
-    exit -1
-fi
-
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
@@ -18,12 +12,25 @@ file_env() {
 	val="$2"
 
     echo "Expanding $val secret into $var"
-    ls $val # Stop execution if the file doesn't exist.
 	export $var=$(cat $val)
 }
 
-file_env 'POSTGRES_USER' '/run/secrets/postgres-user'
-file_env 'POSTGRES_PASS' '/run/secrets/postgres-pass'
+#  Docker secrets take precedence.
+if [ -n "$POSTGRES_USER_FILE" ] ; then
+    echo "POSTGRES_USER_FILE env provided so using it is a secret"
+    file_env 'POSTGRES_USER_FILE' '/run/secrets/postgres-user'
+fi
+
+if [ -n "$POSTGRES_PASSWORD_FILE" ] ; then
+    echo "POSTGRES_PASSWORD_FILE env provided so using it is a secret"
+
+    file_env 'POSTGRES_PASSWORD_FILE' '/run/secrets/postgres-pass'
+fi
+
+if [ -z "$POSTGRES_USER" ]  || [ -z "$POSTGRES_PASSWORD" ] ; then
+    echo "no POSTGRES_USER or POSTGRES_PASSWORD provided as an env or docker secret"
+    exit 1 # terminate and indicate error
+fi
 
 echo "Testing db connection to host:$POSTGRES_HOSTNAME:$POSTGRES_PORT."
 MAX_RETRY=480
@@ -40,10 +47,17 @@ do
         >&2 echo "No connection after $MAX_RETRY seconds"
         exit 1
     fi
-    echo "Retrying..."
+    echo "Retrying db connection host:$POSTGRES_HOSTNAME port:$POSTGRES_PORT ..."
     sleep 1
 done
 
 
-export CATALINA_OPTS="-Dpostgres.hostname=${POSTGRES_HOSTNAME} -Dpostgres.port=${POSTGRES_PORT} -Dpostgres.user=${POSTGRES_USER} -Dpostgres.pass=${POSTGRES_PASS}"
+mkdir -p /home/SMARTconnect/filestore/dataqueue/
+if [ $? -ne 0 ]; then
+    >&2 echo "Couldn't create dataque folder"
+    exit -1
+fi
+
+
+export CATALINA_OPTS="-Dpostgres.hostname=${POSTGRES_HOSTNAME} -Dpostgres.port=${POSTGRES_PORT} -Dpostgres.user=${POSTGRES_USER} -Dpostgres.pass=${POSTGRES_PASSWORD}"
 catalina.sh run
